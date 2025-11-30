5bc40b85cebe5b93172d940e936ed94df987a2ae
syu-cream-glitch <2004mao1029@gmail.com>
2025-11-30 23:47:36 +0900

    knock40:問題なく完了

---
+++ 5.大規模言語モデル/knock40.py
@@ -0,0 +1,25 @@
+import os
+from dotenv import load_dotenv
+import google.generativeai as genai
+
+load_dotenv()
+api_key = os.getenv('GEMINI_API_KEY')
+genai.configure(api_key = api_key)
+
+model = genai.GenerativeModel('gemini-2.5-flash')
+
+prompt = """
+9世紀に活躍した人物に関係するできごとについて述べた次のア～ウを年代の古い順に正しく並べよ。
+
+ア　藤原時平は，策謀を用いて菅原道真を政界から追放した。
+イ　嵯峨天皇は，藤原冬嗣らを蔵人頭に任命した。
+ウ　藤原良房は，承和の変後，藤原氏の中での北家の優位を確立した。
+"""
+
+response = model.generate_content(prompt)
+
+output_file = os.path.join("output", "output40.txt")
+os.makedirs("output", exist_ok = True)
+with open(output_file, "w", encoding = "utf-8") as output_f:
+
+    output_f.write("解答:" + response.text.strip())
