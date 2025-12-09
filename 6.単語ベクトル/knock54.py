dc156ab92141228730587112227617af1bccb4f4
syu-cream-glitch <2004mao1029@gmail.com>
2025-12-09 11:47:53 +0900

    knock54:knock55のためにcsv形式で出力するように修正．

--- 6.単語ベクトル/knock54.py
+++ 6.単語ベクトル/knock54.py
@@ -1,6 +1,7 @@
 import os
 from dotenv import load_dotenv
 from gensim.models import KeyedVectors
+import pandas as pd
 
 load_dotenv()
 path = os.getenv('W2V_MODEL_PATH')
@@ -8,7 +9,7 @@ wv = KeyedVectors.load_word2vec_format(path, binary=True)
 
 input_file = 'data/questions-words.txt'
 os.makedirs('output', exist_ok=True)
-output_file = os.path.join('output', 'output54.txt')
+output_file = os.path.join('output', 'output54.csv')
 
 def section_judge(text, keys):
     lines = text.splitlines()
@@ -25,15 +26,26 @@ def section_judge(text, keys):
     
     return result_list
 
-
-with open(input_file, 'r', encoding='utf-8') as input_f, \
-    open(output_file, 'w', encoding='utf-8') as output_f:
+records = []
+with open(input_file, 'r', encoding='utf-8') as input_f:
     text = input_f.read()
     target_lines = section_judge(text, 'capital-common-countries')
     for line in target_lines[1:]:
         words = line.split()
         most_similar1 = wv.most_similar(positive=[words[1], words[2]], negative=[words[0]], topn=1)
-        output_f.write(f"事例:{line}, 正解単語:{words[3]}, 求めた単語・類似度:{most_similar1[0][0]}・{most_similar1[0][1]}\n")
+        predicted_word, similarity = most_similar1[0]
+        records.append({
+            'word1': words[0],
+            'word2': words[1],
+            'word3': words[2],
+            'answer': words[3],
+            'predicted': predicted_word,
+            'correct': predicted_word == words[3],
+            'similarity': similarity
+        })
+    df = pd.DataFrame(records)
+    df.to_csv(output_file, index=False, encoding='utf-8-sig')
+
 
 
 
