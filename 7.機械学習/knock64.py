bba0e2fd8b4a5c3320898e99f8db6a8fe557d86c
syu-cream-glitch <2004mao1029@gmail.com>
2025-12-14 15:23:59 +0900

    knock64:ほとんどknock63と同じだが，predict_probaで条件付き確率を予測．

---
+++ 7.機械学習/knock64.py
@@ -0,0 +1,38 @@
+import os
+import pandas as pd
+from joblib import load
+from collections import Counter
+
+input_files = [
+    "SST-2/train.tsv",
+    "SST-2/dev.tsv"
+]
+os.makedirs("output", exist_ok=True)
+output_file = os.path.join("output", "output64.txt")
+
+dfs = [pd.read_csv(f, sep="\t") for f in input_files]
+
+def text_to_feature(text):
+    tokens = text.split()
+    return dict(Counter(tokens))
+
+def df_to_feature_list(df):
+    feature_list = []
+    for _, row in df.iterrows():
+        feature_list.append({
+            'text': row['sentence'],
+            'label': row['label'],
+            'feature': text_to_feature(row['sentence'])
+        })
+    return feature_list
+
+# 学習データはknock63では使用しない
+# train_data = df_to_feature_list(dfs[0])
+dev_data = df_to_feature_list(dfs[1])
+
+lr_loaded, vec_loaded = load("output/output62.joblib")
+X_new = vec_loaded.transform([dev_data[0]['feature']])
+proba = lr_loaded.predict_proba(X_new)
+
+with open(output_file, "w", encoding="utf-8") as output_f:
+    output_f.write(f"Predicted probabilities:{proba}")
