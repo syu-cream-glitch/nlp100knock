97eb955a3f623d1b5b5aa6d9e26ffd4c7285da71
syu-cream-glitch <2004mao1029@gmail.com>
2025-12-14 12:24:35 +0900

    knock60:value_counts()を用いて簡略化した．

--- 7.機械学習/knock60.py
+++ 7.機械学習/knock60.py
@@ -10,21 +10,10 @@ output_file = os.path.join("output", "output60.txt")
 
 dfs = [pd.read_csv(f, sep="\t") for f in input_files]
 
-def Count_PandN(df):
-    cntP = 0
-    cntN = 0
-    for _, raw in df.iterrows():
-        if raw["label"] == 1:
-            cntP += 1
-        elif raw["label"] == 0:
-            cntN += 1
-    
-    return cntP, cntN
-
 with open(output_file, "w", encoding="utf-8") as output_f:
-    for fname, df in zip(["train.tsv","dev.tsv"], dfs):
-        cntP, cntN = Count_PandN(df)
-        output_f.write(f"{fname}\npositive:{cntP}\tnegative:{cntN}\n")
+    for fname, df in zip(["train.tsv", "dev.tsv"], dfs):
+        output_f.write(f"{fname}\n")
+        output_f.write(df["label"].value_counts().to_string() + "\n")
 
 
 
